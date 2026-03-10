53ffea86faf22b30cac107eeb8a8ee7e0be02a7a
MAEAGLE\alexisb <alexisb@shop.com>
2026-03-10 15:27:16 -0700

    button disabling by dosage

--- custom-cocktail-widget/src/custom-cocktail-builder/CocktailBuilderProdIngredient.tsx
+++ custom-cocktail-widget/src/custom-cocktail-builder/CocktailBuilderProdIngredient.tsx
@@ -15,9 +15,9 @@ export const CocktailBuilderProdIngredient: React.FC<ProductPurchaseProps> = ({
       <img className={"cc-builder__prod-img"} alt={prodIngredient.name} src={prodIngredient.imageUrl} />
       <div className={"cc-builder__prod-letter"}>{prodIngredient.letter}</div>
       <div className={"cc-builder__prod-action"}>
-        <button className={"cc-builder__prod-action-btn cc-builder__prod-action-btn--subtract"}></button>
+        <button className={`cc-builder__prod-action-btn cc-builder__prod-action-btn--subtract ${prodIngredient.dosesSelected === 0 ? 'cc-builder__prod-action-btn--disabled' : ''}`}/>
         <div className={"cc-builder__prod-count"}>{prodIngredient.dosesSelected}</div>
-        <button className={"cc-builder__prod-action-btn cc-builder__prod-action-btn--add"}></button>
+        <button className={`cc-builder__prod-action-btn cc-builder__prod-action-btn--add ${prodIngredient.dosesSelected === prodIngredient.maxDoses ? 'cc-builder__prod-action-btn--disabled' : ''}`}/>
       </div>
       <div></div>
     </div>
