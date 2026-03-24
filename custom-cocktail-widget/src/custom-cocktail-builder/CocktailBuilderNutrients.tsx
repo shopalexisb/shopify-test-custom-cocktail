eeaad6226dd31a60e76a1dee329c6304d0a3c320
MAEAGLE\alexisb <alexisb@shop.com>
2026-03-24 14:48:28 -0700

    handle no nutrients (greens)

--- custom-cocktail-widget/src/custom-cocktail-builder/CocktailBuilderNutrients.tsx
+++ custom-cocktail-widget/src/custom-cocktail-builder/CocktailBuilderNutrients.tsx
@@ -9,16 +9,22 @@ export const CocktailBuilderNutrients: React.FC<ProductPurchaseProps> = ({
                                                                            nutrients
                                                                 }) => {
 
+  const filteredNutrients = nutrients.filter(nutrient => nutrient.percentage > 0);
+
   return (
     <div className={"cc-builder__nutrient-widget"}>
-      {nutrients
-        .filter(nutrient => nutrient.percentage > 0)
-        .map((nutrient, index) => (
+      {filteredNutrients.length === 0 ? (
+        <div className={"cc-builder__nutrient-widget-text"}>
+          No significant nutrients for this product
+        </div>
+      ) : (
+        filteredNutrients.map((nutrient, index) => (
           <div className={"cc-builder__nutrient-widget-text"} key={index}>
             <span>{nutrient.name}</span>
             <span>{nutrient.percentage}%</span>
           </div>
-        ))}
+        ))
+      )}
     </div>
   );
 };
